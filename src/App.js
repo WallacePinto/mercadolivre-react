d50d30e3d16dfa8a53e3feebda05b8b7f758751c
Wallace <wallaceopinto@gmail.com>
2020-08-10 21:58:36 -0300

    Primeiro commit, primeiro dia de code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
+import  styled from 'styled-components';
 import './App.css';
+import Logo from './assets/img/logo.png'
+import Carousel from "./Carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const DivBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+const HeaderContainer = styled.header`
+  display: flex;
+  flex-direction: column;
+  background-color: #fff159;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  margin-top: 8px;
+  border-bottom: 1px solid gray;
+`;
+
+const LogoImg = styled.img`
+  min-width: 44px;
+  height: 32px;
+  display: flex;
+  margin-right: 12px;
+  margin-left: 12px;
+`
+
+const Input = styled.input`
+  min-width: 326px;
+  height: 33px; 
+  margin-right: 12px;
+`;
+
+const IconsNav = styled.p`
+  min-width: 45px;
+  height: 50px;
+  align-items: center;
+  justify-content: center;
+  margin-right: 12px;
+`;
+
+const InfoCep = styled.a`
+  text-decoration: none;
+  cursor: pointer;
+  color: #000;
+  margin-left: 12px;
+  padding: 5px;
+  font-size: 12px;
+`;
+
+const PaymentMenu = styled.section`
+  background: #ebebeb;
+  display: flex;
+  flex-grow: 1;
+`;
+
+const MainDiv = styled.div`
+  background: #FFF;
+  display: flex;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DivBody>
+    <HeaderContainer className="App">
+        <Nav>
+          <LogoImg src={Logo}/>
+          <Input placeholder="Buscar produtos, marcos e muito mais ..."/>
+          <IconsNav>M</IconsNav>
+          <IconsNav>S</IconsNav>
+        </Nav>
+        
+        <InfoCep href="#"> Informe seu CEP</InfoCep>
+        <Carousel />
+    </HeaderContainer>    
+
+    <PaymentMenu>
+      <MainDiv>
+
+      </MainDiv>
+    </PaymentMenu>
+    </DivBody>
   );
 }
 
---
+++ src/assets/img/Payment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  styled from 'styled-components';
+
+function Payment() {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Payment;
